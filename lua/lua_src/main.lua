e0b7e3363756f9d37156e4c113035ee391ad8363
evannnns <alt1@trickster.pw>
2022-10-25 09:31:00 +0800

    add lua scripting suport for mandelbrot

---
+++ lua/lua_src/main.lua
@@ -0,0 +1,36 @@
+http = require("socket.http")
+-- http = require 'socket.http'
+ltn12 = require("ltn12")
+-- ltn12 = require 'ltn12'
+  
+local osinttb = {}
+
+osinttb = setmetatable(osinttb, v) -- assume v is iteratable
+
+for osintb,v in pairs(osint) do 
+    osinttb = __index(function(v))
+        return ... v ... "table 1 declared"
+    end
+end
+
+return osinttb
+
+function logu(url)
+    print("target user url (cannot be private): ")
+    url = io.read()   
+    for k,v in pairs(url) do
+        table.insert(url..."\n"..., osinttb)
+    until osinttb[1] < #osinttb
+        table.split(osinttb, "\r\n")
+        print(osinttb)
+    -- http part: fire request
+    http.get(url, "?theme=dark")
+    wait(len(url.response - url.request))
+    url.response.body = function()
+        respb = url.response.body 
+        io.write(respb, "/src/output1.html")
+        table.split(osinttb)
+        osinttb = __newindex(v, respb, "\r\n")
+        if osinttb.__index(v) ~= osinttb.
+        -- return output and write it to html with proper syntacticality
+    end
